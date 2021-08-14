d44dae05f6932cf39e3d81b624c540d68b61c207
Pavel Zharov <pavel@prna.ru>
2021-08-15 00:30:15 +0700

    feat: :sparkles: add poll api request

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,26 @@
 class Api {
-  constructor({market}) {
-    const fetch = fetch(`./api/v1/${market}`, {
+  _request(path) {
+    return fetch(`./api/v1/${path}`, {
       method: 'GET'
     })
-      .theh(res => {
+      .then(res => {
         if (res.ok) {
           return res.json();
         }
         return Promise.reject(`Error ${res.status}: ${res.nessage}`)
       })
   }
+
+  getMarketData = (market) => {
+    return this._request(market)
+  }
+
+
+  getMarketPollData = (market) => {
+    return this._request(`${market}/poll`)
+  }
 }
 
-export default Api;
+const api = new Api();
+
+export default api;
